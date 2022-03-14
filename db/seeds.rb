325be80d93f343cea2f77836fa1798607107a817
Zhicong Jiang <terence592592@gmail.com>
2022-03-14 15:05:01 +0000

    test account

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.where(email:'test@test.com').first_or_create(password:'test', password_confirmation:'test')
+User.where(email:'test@test.com').first_or_create(password:'testtest', password_confirmation:'testtest')
