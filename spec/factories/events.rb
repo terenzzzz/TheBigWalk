985fbe9b02a35aa311980ef26ede822e273b4bbe
Ben Dawson <bcdawson1@sheffield.ac.uk>
2022-03-30 09:31:30 +0100

    Created leaderboard page and added Participant, Marshal and Admin tags

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -2,14 +2,14 @@
 #
 # Table name: events
 #
-#  id                     :bigint           not null, primary key
-#  name                   :string
-#  date                   :date           
-#  time                   :time
-#  length                 :integer
-#  eventID                :integer
-#  created_at             :datetime         not null
-#  updated_at             :datetime         not null
+#  id         :bigint           not null, primary key
+#  date       :date
+#  eventID    :integer
+#  length     :integer
+#  name       :string
+#  time       :time
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
 #
 FactoryBot.define do
     factory :event do
