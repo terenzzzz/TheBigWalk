8cb167fa7e6c99d0b9feb727de93f508b36aad7f
Raluca Cruceriu <rcruceriu1@sheffield.ac.uk>
2022-03-30 10:34:45 +0100

    Added Foreign keys 2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_28_102817) do
+ActiveRecord::Schema.define(version: 2022_03_30_092425) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,8 @@ ActiveRecord::Schema.define(version: 2022_03_28_102817) do
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "users_id", null: false
+    t.index ["users_id"], name: "index_admins_on_users_id"
   end
 
   create_table "checkpoints", force: :cascade do |t|
@@ -87,13 +89,16 @@ ActiveRecord::Schema.define(version: 2022_03_28_102817) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "marshals", force: :cascade do |t|
+  create_table "marshalls", force: :cascade do |t|
     t.integer "marshal_id"
     t.integer "checkPoint_id"
     t.integer "user_id"
-    t.integer "check_point_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "checkpoints_id", null: false
+    t.bigint "users_id", null: false
+    t.index ["checkpoints_id"], name: "index_marshalls_on_checkpoints_id"
+    t.index ["users_id"], name: "index_marshalls_on_users_id"
   end
 
   create_table "participants", force: :cascade do |t|
@@ -107,6 +112,12 @@ ActiveRecord::Schema.define(version: 2022_03_28_102817) do
     t.integer "check_point_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "checkpoints_id", null: false
+    t.bigint "users_id", null: false
+    t.bigint "routes_id", null: false
+    t.index ["checkpoints_id"], name: "index_participants_on_checkpoints_id"
+    t.index ["routes_id"], name: "index_participants_on_routes_id"
+    t.index ["users_id"], name: "index_participants_on_users_id"
   end
 
   create_table "routes", force: :cascade do |t|
@@ -114,6 +125,8 @@ ActiveRecord::Schema.define(version: 2022_03_28_102817) do
     t.string "length"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "checkpoints_id", null: false
+    t.index ["checkpoints_id"], name: "index_routes_on_checkpoints_id"
   end
 
   create_table "sessions", force: :cascade do |t|
@@ -157,5 +170,12 @@ ActiveRecord::Schema.define(version: 2022_03_28_102817) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "admins", "users", column: "users_id"
   add_foreign_key "checkpoints", "events", column: "events_id"
+  add_foreign_key "marshalls", "checkpoints", column: "checkpoints_id"
+  add_foreign_key "marshalls", "users", column: "users_id"
+  add_foreign_key "participants", "checkpoints", column: "checkpoints_id"
+  add_foreign_key "participants", "routes", column: "routes_id"
+  add_foreign_key "participants", "users", column: "users_id"
+  add_foreign_key "routes", "checkpoints", column: "checkpoints_id"
 end
