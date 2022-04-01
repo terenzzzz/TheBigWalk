7c9006d88d34d2b77861d3751d789b66142eab3c
Raluca Cruceriu <rcruceriu1@sheffield.ac.uk>
2022-04-01 13:07:38 +0100

    commit to let me merge

--- spec/models/route_spec.rb
+++ spec/models/route_spec.rb
@@ -2,11 +2,20 @@
 #
 # Table name: routes
 #
-#  id         :bigint           not null, primary key
-#  length     :string
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#  route_id   :integer
+#  id             :bigint           not null, primary key
+#  length         :string
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  checkpoints_id :bigint           not null
+#  route_id       :integer
+#
+# Indexes
+#
+#  index_routes_on_checkpoints_id  (checkpoints_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (checkpoints_id => checkpoints.id)
 #
 require 'rails_helper'
 
