0181ce1450f6592d3712898b7abe437f10d3b01e
Zhicong Jiang <terence592592@gmail.com>
2022-04-05 14:34:58 +0100

    Add a pop up for delete account and finished delete account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,12 @@ class UsersController < ApplicationController
         end
     end
     
+
+    def destroy
+        @user.destroy
+        redirect_to '/'
+    end
+
     private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
