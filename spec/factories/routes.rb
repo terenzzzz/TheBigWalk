805f3d6a82343a7f5558aa545df23d72fde0a003
Zhicong Jiang <terence592592@gmail.com>
2022-04-12 16:59:49 +0100

    Fixed seed and home page typo and checkin fail code

--- spec/factories/routes.rb
+++ spec/factories/routes.rb
@@ -2,20 +2,22 @@
 #
 # Table name: routes
 #
-#  id             :bigint           not null, primary key
-#  length         :string
-#  created_at     :datetime         not null
-#  updated_at     :datetime         not null
-#  checkpoints_id :bigint           not null
-#  route_id       :integer
+#  id            :bigint           not null, primary key
+#  course_length :integer
+#  name          :string
+#  start_date    :date
+#  start_time    :time
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  events_id     :bigint           not null
 #
 # Indexes
 #
-#  index_routes_on_checkpoints_id  (checkpoints_id)
+#  index_routes_on_events_id  (events_id)
 #
 # Foreign Keys
 #
-#  fk_rails_...  (checkpoints_id => checkpoints.id)
+#  fk_rails_...  (events_id => events.id)
 #
 FactoryBot.define do
   factory :route do
