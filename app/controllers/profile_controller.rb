e0c8c7a41f71c6cc830776e8394487d4b828b2e2
Zhicong Jiang <terence592592@gmail.com>
2022-04-28 19:15:04 +0100

    fix walker profile

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -4,6 +4,10 @@ class ProfileController < ApplicationController
     def account
     end
     
+    def index
+        @route_id = session[:current_route_id]
+        @participant_id = Participant.where(routes_id: @route_id, user_id: current_user.id).first.id
+    end
 
     private
     # Use callbacks to share common setup or constraints between actions.
