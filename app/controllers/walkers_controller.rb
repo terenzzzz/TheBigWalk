b7cdc28b38dd531f98eb1f316ba0ede88ce80464
Ben Dawson <bcdawson1@sheffield.ac.uk>
2022-04-30 14:53:19 +0100

    Participant table upadting correctly

--- app/controllers/walkers_controller.rb
+++ app/controllers/walkers_controller.rb
@@ -49,19 +49,22 @@ class WalkersController < ApplicationController
     end
 
     def sign_up_participant
-      participant = Participant.where(routes_id:session[:current_route_id]).first_or_create(checkpoints_id:"1", routes_id: session[:current_route_id], user_id: current_user.id, event_id: session[:current_event_id])
+      participant = Participant.where(routes_id:session[:current_route_id], user_id:session[:current_user_id]).first_or_create(checkpoints_id:"1", routes_id: session[:current_route_id], user_id: current_user.id, event_id: session[:current_event_id], rank: 0, opted_in_leaderboard: session[:opted_in])
       participant.save
+      # @current_participant_opted_in = Participant.where(routes_id:session[:current_route_id], user_id: session[:current_user_id]).first.opted_in_leaderboard #current_user.id).first.opted_in_leaderboard
+      # puts "$$$$$ #{@current_participant_opted_in}"
+      # if @current_participant_opted_in #== true
+      #   participant.update(opted_in_leaderboard: true)
+      #   puts "676767"
+      # else
+      #   participant.update(opted_in_leaderboard: false)
+      #   puts "565656"
+      # end
+      puts "current user opted in: #{participant.opted_in_leaderboard} + #{Participant.where(routes_id:session[:current_route_id], user_id:session[:current_user_id]).first_or_create(checkpoints_id:"1", routes_id: session[:current_route_id], user_id: current_user.id, event_id: session[:current_event_id]).id}"
 
-      puts "current user opted in: #{participant.opted_in_leaderboard}"
-      @current_participant_opted_in = Participant.where(user_id: current_user.id).first.opted_in_leaderboard
-      if @current_participant_opted_in == true
-        participant.update(opted_in_leaderboard: true)
-      else
-        participant.update(opted_in_leaderboard: false)
-      end
       puts "And again 1: #{@current_participant_opted_in} \n\n"
       puts "Participant in table: #{Participant.where(routes_id:session[:current_route_id]).first_or_create(checkpoints_id:"1", routes_id: session[:current_route_id], user_id: current_user.id, event_id: session[:current_event_id])}"
-
+      #participant.save
       if participant.save
         redirect_to walkers_path
       else
@@ -96,7 +99,6 @@ class WalkersController < ApplicationController
       @wgs84_point = OsgbConvert::WGS84.new(@lat, @lon, 0)
       @osUKgridPoint = OsgbConvert::OSGrid.from_wgs84(@wgs84_point)
       @osReference = @osUKgridPoint.grid_ref(6)
-      
     end
 
     def help
@@ -123,12 +125,12 @@ class WalkersController < ApplicationController
 
     def index
       user = User.where(id: session[:current_user_id]).first
-      puts "User: #{user}"
+      puts "User: #{user.id}"
       @walker = Participant.where(user_id: user.id).first
       puts "Route ID: #{@walker}"
       checkpoint_pos = RoutesAndCheckpointsLinker.where(route_id: @walker.routes_id, checkpoint_id: @walker.checkpoints_id).first.position_in_route
-      @linker = RoutesAndCheckpointsLinker.where(position_in_route: (checkpoint_pos + 1), route_id: @walker.routes_id).first
-      @checkpoint = Checkpoint.where(id: @linker.checkpoint_id).first
+      linker = RoutesAndCheckpointsLinker.where(position_in_route: (checkpoint_pos + 1), route_id: @walker.routes_id).first
+      @checkpoint = Checkpoint.where(id: linker.checkpoint_id).first
 
       @advisedTime = @linker.advised_time
       @time = CheckpointTime.where(checkpoint_id: @walker.checkpoints_id, participant_id: @walker.id).first.times
