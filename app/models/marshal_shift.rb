d007f2856b28a6660db6b48594cdbd8408ecc59d
Evan Quinn <equinn3@sheffield.ac.uk>
2022-05-04 00:59:00 +0100

    commit cus stuff is broke

---
+++ app/models/marshal_shift.rb
@@ -0,0 +1,23 @@
+# == Schema Information
+#
+# Table name: marshalls
+#
+#  id             :bigint           not null, primary key
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  checkpoints_id :bigint
+#  marshal_id     :integer
+#  users_id       :bigint           not null
+#
+# Indexes
+#
+#  index_marshalls_on_checkpoints_id  (checkpoints_id)
+#  index_marshalls_on_users_id        (users_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (checkpoints_id => checkpoints.id)
+#  fk_rails_...  (users_id => users.id)
+#
+class MarshalShift < ApplicationRecord
+end
