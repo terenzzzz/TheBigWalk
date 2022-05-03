76029f7747c9fdd2e9dfdac7bcd297701db2337f
Zhicong Jiang <terence592592@gmail.com>
2022-05-03 12:24:23 +0100

    Edit marshal_id now works

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
             
             #Edit User's Marshal_Id
             elsif Tag.where(id: @user.tag_id).first.name == "Marshal"
-                marshal = Marshall.where(routes_id: session[:current_route_id], users_id: @user.id).first
+                marshal = Marshall.where(checkpoints_id: session[:current_checkpoint_id], users_id: @user.id).first
                 marshal.update(params.require(:marshall).permit(:marshal_id))
             end
             redirect_to profile_index_path, notice: 'Profile successfully updated.'
