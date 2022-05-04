499505148340981d15e37e9d653db29e0ccca0c1
Ben Dawson <bcdawson1@sheffield.ac.uk>
2022-05-04 23:30:47 +0100

    Got the leaderboard working on the view user page, which can be accessed by anyone even when not signed in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,28 +10,13 @@ class UsersController < ApplicationController
         end
         session[:reset_route] = 0
 
-        #####
-
-        puts "@@@@@ CUrrent route id: #{session[:current_route_id]}"
-        @leaderboard_user_id=params[:leaderboard_user_id]
-
-        @participant = Participant.where(user_id: @leaderboard_user_id).first
-        @leaderboard_user = User.where(id: @leaderboard_user_id).first
-        puts "*****&& #{@leaderboard_user}"
+        @participant = Participant.where(user_id: @user.id).first
         @current_route_id=session[:current_route_id]
-        @route_checkpoints = Array.new 
+        @route_checkpoints = Array.new
         
-        # CURRENTLY THIS \/ is not returning anything (length = 0)
-        # Since it works in the normal single user leaderboard, there must be something
-        #    that is not working. Maybe the current user is needed 
         @routes_linker = RoutesAndCheckpointsLinker.where(route_id: session[:current_route_id])
-        puts "::::: %%% #{@routes_linker.length()}"
         @checkpoints_for_route = Checkpoint.where(id: @routes_linker)
-        puts "¢¢¢ RACL #{@routes_linker.length()} ++ #{@checkpoints_for_route.length()}"
         @route_checkpoints.concat @checkpoints_for_route
-        puts "&&&&&&&&&^^^^^^ #{@route_checkpoints}"
-
-        #####
 
     end
 
@@ -39,7 +24,8 @@ class UsersController < ApplicationController
     end
 
     def route_picked
-        session[:current_route_id] = params.require(:route_picked).permit(:route_id)
+        current_route = params.require(:route_picked).permit(:route_id)
+        session[:current_route_id] = current_route[:route_id]
         user = User.where(id: session[:view_user_id]).first
         session[:reset_route] = 1
         redirect_to user
