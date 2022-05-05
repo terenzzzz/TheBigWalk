9bcffd8fe5e76a1f07d44032218deee6772d6b9d
Evan Quinn <equinn3@sheffield.ac.uk>
2022-05-05 02:12:55 +0100

    trying to fix issue

--- app/controllers/walkers_controller.rb
+++ app/controllers/walkers_controller.rb
@@ -287,12 +287,11 @@ class WalkersController < ApplicationController
     #params_id = params[:id]
     #session[:current_route_id] = params_id[:route_id]
 
-    route_id = params[:id]
-    session[:current_route_id] = route_id[:id]
+    
     #####
     user = User.where(id: session[:current_user_id]).first
     puts "User: #{user.id}"
-    @walker = Participant.where(user_id: user.id, routes_id: session[:current_route_id]).first
+    @walker = Participant.where(user_id: user.id, routes_id: params[:id]).first
     puts "Route ID: #{@walker}"
     
     checkpoint_pos = RoutesAndCheckpointsLinker.where(route_id: @walker.routes_id, checkpoint_id: @walker.checkpoints_id).first.position_in_route
