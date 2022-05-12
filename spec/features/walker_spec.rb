5e33adbb2773be255ca94d50e0aa8899a8de2aad
Zhicong Jiang <terence592592@gmail.com>
2022-05-12 13:45:22 +0100

    Get walker testing working

--- spec/features/walker_spec.rb
+++ spec/features/walker_spec.rb
@@ -4,20 +4,19 @@ describe 'walker' do
     let!(:event) { FactoryBot.create(:event, :public_event) }
     let!(:route) { FactoryBot.create(:route, event: event) }
 
-    let!(:checkpoint1) { Checkpoint.create(name: "Ladybower Reservoir/A6013 main road", os_grid: 'SK331896', event: event)}
-    let!(:linker1) { RoutesAndCheckpointsLinker.create(distance_from_start: '0',checkpoint_description: "Head to the main road (A6187)",advised_time: 20,route: route,checkpoint: checkpoint1,position_in_route: '1')}
+    let!(:checkpoint1) { Checkpoint.create(name: "test1", os_grid: 'SK331896', event: event)}
+    let!(:linker1) { RoutesAndCheckpointsLinker.create(distance_from_start: '10',checkpoint_description: "Head to the main road (A6187)",advised_time: 20,route: route,checkpoint: checkpoint1,position_in_route: '1')}
     
-    let!(:checkpoint2) { Checkpoint.create(name: "Ladybower Reservoir/A6013 main road", os_grid: 'SK331896', event: event)}
-    let!(:linker2) { RoutesAndCheckpointsLinker.create(distance_from_start: '0',checkpoint_description: "Head to the main road (A6187)",advised_time: 20,route: route,checkpoint: checkpoint2,position_in_route: '2')}
+    let!(:checkpoint2) { Checkpoint.create(name: "test2", os_grid: 'SK331896', event: event)}
+    let!(:linker2) { RoutesAndCheckpointsLinker.create(distance_from_start: '50',checkpoint_description: "Head to the main road (A6187)",advised_time: 20,route: route,checkpoint: checkpoint2,position_in_route: '2')}
     
-    let!(:checkpoint3) { Checkpoint.create(name: "Ladybower Reservoir/A6013 main road", os_grid: 'SK331896', event: event)}
-    let!(:linker3) { RoutesAndCheckpointsLinker.create(distance_from_start: '0',checkpoint_description: "Head to the main road (A6187)",advised_time: 20,route: route,checkpoint: checkpoint3,position_in_route: '3')}
+    let!(:checkpoint3) { Checkpoint.create(name: "test3", os_grid: 'SK331896', event: event)}
+    let!(:linker3) { RoutesAndCheckpointsLinker.create(distance_from_start: '100',checkpoint_description: "Head to the main road (A6187)",advised_time: 20,route: route,checkpoint: checkpoint3,position_in_route: '3')}
     
 
     let!(:tag1) { FactoryBot.create(:tag) }
     let!(:user1) {  FactoryBot.create(:user, name:'test', email: 'test@test.com', mobile:'00000000000', tag: tag1)}
-    # let!(:participant) {FactoryBot.create(:participant, user:user1, route:route, checkpoint:checkpoint1, event:event)}
-    let!(:opted_in) { OptedInLeaderboard.create(opted_in:true,user_id:user1) }
+    let!(:opted_in) { OptedInLeaderboard.where(user:user1).first_or_create(opted_in: true) }
     
 
     
