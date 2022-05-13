082d9d2d6620e5c3a1a837928893bfaa8904225f
Thomas <tstuart2@sheffield.ac.uk>
2022-05-13 02:23:59 +0100

    barely any progress made as I can't test tests

--- spec/features/marshall_spec.rb
+++ spec/features/marshall_spec.rb
@@ -1,27 +1,33 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# describe 'checkpoints' do
-#     let(:tag1) { FactoryBot.create(:tag, :marshall) }
-#     let(:user1) { FactoryBot.create(:user, name:'test', email: 'test@test.com', mobile:'00000000000', tag: tag1) }
+describe 'checkpoints' do
+    let!(:event) { FactoryBot.create(:event, :public_event) }
+    let!(:route) { FactoryBot.create(:route, event: event) }
 
-#     let!(:event) { FactoryBot.create(:event, :public_event) }
-#     let!(:route) { FactoryBot.create(:route, event: event) }
-#     let!(:checkpoint1) { FactoryBot.create(:checkpoint, event: event, id: '1') }
-#     let!(:checkpoint2) { FactoryBot.create(:checkpoint, event: event, id: '2') }
+    let!(:checkpoint1) { Checkpoint.create(name: "test1", os_grid: 'SK331896', event: event)}
+    let!(:linker1) { RoutesAndCheckpointsLinker.create(distance_from_start: '10',checkpoint_description: "Head to the main road (A6187)",advised_time: 20,route: route,checkpoint: checkpoint1,position_in_route: '1')}
+    
+    let!(:checkpoint2) { Checkpoint.create(name: "test2", os_grid: 'SK331896', event: event)}
+    let!(:linker2) { RoutesAndCheckpointsLinker.create(distance_from_start: '50',checkpoint_description: "Head to the main road (A6187)",advised_time: 20,route: route,checkpoint: checkpoint2,position_in_route: '2')}
+    
+    let!(:checkpoint3) { Checkpoint.create(name: "test3", os_grid: 'SK331896', event: event)}
+    let!(:linker3) { RoutesAndCheckpointsLinker.create(distance_from_start: '100',checkpoint_description: "Head to the main road (A6187)",advised_time: 20,route: route,checkpoint: checkpoint3,position_in_route: '3')}
+    
+    let(:tag1) { FactoryBot.create(:tag, :marshall) }
+    let(:user1) { FactoryBot.create(:user, name:'test', email: 'test@test.com', mobile:'00000000000', tag: tag1) }
+    let!(:walker1) { FactoryBot.create(:participant)}
 
-#     context 'As a marshal' do
-#         before do
-#             login_as user1
+    context 'As a marshal' do
+         before do
+             login_as user1
+         end
 
-#             RoutesAndCheckpointsLinker.create(distance_from_start: 3, advised_time: 30, checkpoint: checkpoint1, route: route, position_in_route: 1)
-#             RoutesAndCheckpointsLinker.create(distance_from_start: 3, advised_time: 30, checkpoint: checkpoint2, route: route, position_in_route: 2)
-#             RoutesAndCheckpointsLinker.create(distance_from_start: 3, advised_time: 30, checkpoint: checkpoint3, route: route, position_in_route: 3)
-#         end
-
-#         #Tom will finish all this later
+         #Tom will finish all this later
 
 #         specify "I can sign in to an event"
-#             visit "/"
+#             visit '/'
+#             click_on event.name
+#             expect(page).to have_content 'Choose Your Current Checkpoint'
 #             #go to choose_event
 
 #         specify "I can sign in to a checkpoint"
@@ -40,7 +46,7 @@
 #             visit "/"
 #             click_link 'View Incoming Walkers'
         
-        #This one is old and definitely needs editing
+#        #This one is old and definitely needs editing
 #        specify "I can checkin a walker to my checkpoint" do
 #            FactoryBot.create :participant
 #            visit "/"
@@ -82,9 +88,9 @@
 #             click_link 'End Marshal Shift'
 #             click_link 'End For The Day'
 
-#         #There probably is more to add but I can't think of them right now
-#         #Again - THESE ARE MOST CERTAINLY NOT DONE, I will do it in the next couple of days - Tom
+         #There probably is more to add but I can't think of them right now
+         #Again - THESE ARE MOST CERTAINLY NOT DONE, I will do it in the next couple of days - Tom
 
-#     end
+    end
 
-# end
+end
